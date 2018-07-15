6802d6d00574af9f7130b6b006af40027e2ac131
Maxime Michel <maxime-michel@users.noreply.github.com>
2018-07-15 18:44:30 +0200

    roll changes back

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -13,7 +13,6 @@ c.content.media_capture = False
 # looks
 c.qt.highdpi = True
 c.tabs.title.format = "{title}"
-c.fonts.hints = 'bold 14pt monospace'
 c.completion.height = "30%"
 c.completion.web_history_max_items = 1000
 
@@ -46,3 +45,16 @@ c.colors.tabs.selected.odd.bg = 'white'
 c.colors.tabs.selected.even.fg = 'black'
 c.colors.tabs.selected.even.bg = 'white'
 c.colors.tabs.even.bg = 'grey'
+
+# fonts
+c.fonts.completion.entry = c.fonts.tabs
+c.fonts.debug_console = c.fonts.tabs
+c.fonts.downloads = c.fonts.tabs
+c.fonts.keyhint = c.fonts.tabs
+c.fonts.messages.error = c.fonts.tabs
+c.fonts.messages.info = c.fonts.tabs
+c.fonts.messages.warning = c.fonts.tabs
+c.fonts.statusbar = c.fonts.tabs
+c.fonts.hints = 'bold 14pt monospace'
+c.fonts.completion.category = c.fonts.hints
+c.fonts.prompts = '14pt sans-serif'
