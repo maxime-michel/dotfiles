366477a7f1788bd705373db74c60398b1c2b3167
Maxime Michel <maxime-michel@users.noreply.github.com>
2020-03-16 10:28:30 +0100

    Mention zsh plugins in install.sh

--- install.sh
+++ install.sh
@@ -11,3 +11,4 @@ mkdir ~/.tmp
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# plugins=(git git-prompt fzf)
